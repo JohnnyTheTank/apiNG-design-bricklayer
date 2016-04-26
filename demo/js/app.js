5711acafea078618aebde07a01d83936b12ebe5a
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-04-26 17:35:14 +0200

    demo: small improvement

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('app', ['jtt_aping', 'jtt_aping_flickr', 'jtt_aping_design_bricklayer']);
+angular.module('app', ['jtt_aping', 'jtt_aping_flickr', 'jtt_aping_design_bricklayer']);
